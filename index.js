76f0f551f8d2b475183935d62f9e2d8e694486e3
Kevin Huang <yellow.khaos@gmail.com>
2018-10-27 14:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function init() {
   //put any page initialization/handlebars initialization here
-
+  Handlebars.registerHelper('displayIngredient', function(){
+    return new Handlebars.SafeString(this.value)
+  })
 
 
 }
