ede8ea989402eda793c0f6e576f069f149d323f3
Kevin Huang <yellow.khaos@gmail.com>
2018-10-27 14:28:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  
-  
+
+
   Handlebars.registerHelper('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerHelper('displayIngredient', function(){
     return new Handlebars.SafeString(this.value)
