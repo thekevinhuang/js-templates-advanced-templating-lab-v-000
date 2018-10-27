509148b329ad603e7cede27f95d3753800ecf02e
Kevin Huang <yellow.khaos@gmail.com>
2018-10-27 14:26:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  let mainHTML = document.getElementById("main") 
+  let mainHTML = document.getElementById("main")
   Handlebars.registerHelper('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerHelper('displayIngredient', function(){
     return new Handlebars.SafeString(this.value)
