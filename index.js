6eed975aec2e41c26f179ee562e247f45eeb2f1c
Kevin Huang <yellow.khaos@gmail.com>
2018-10-27 14:27:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  let mainHTML = document.getElementById("main")
+  let mainHTML = document.getElementById("main").innerHTML
+  
   Handlebars.registerHelper('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerHelper('displayIngredient', function(){
     return new Handlebars.SafeString(this.value)
   })
 
-
+  let initialForm = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+  mainHTML += initialForm()
 }
 
 function createRecipe() {
